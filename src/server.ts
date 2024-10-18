1af3d34fa760672065ee3d38f9d4c89336587ba8
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-10-18 21:29:26 +0100

    add middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 // express
 
 import express from "express"
-
 import router from "./router"
+import morgan from "morgan"
 
 const app = express()
+
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
   console.log("hello from express")
