7eede3edf80056698c2e9fcf7275c13698e7aa2a
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-10-22 15:51:12 +0100

    add express-validator

--- src/router.ts
+++ src/router.ts
@@ -1,37 +1,46 @@
-import { Router } from "express"
+import { Router } from "express";
+import { body, validationResult } from "express-validator";
 
-const router = Router()
+const router = Router();
 
 /**
  * Products
-*/
+ */
 
 router.get("/product", (req, res) => {
-  res.json({message: "hello"})
-})
-router.get("/product/:id", () => {})
-router.put("/product/:id", () => {})
-router.post("/product", () => {})
-router.delete("/product/:id", () => {})
+  res.json({ message: "hello" });
+});
+router.get("/product/:id", () => {});
+router.put("/product/:id", body("name").isString(), (req, res) => {
+  const errors = validationResult(req);
+  console.log(errors);
+
+  if (!errors.isEmpty()) {
+    res.status(400);
+    res.json({ errors: errors.array() });
+  }
+});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
 
 /**
  * Update
-*/
+ */
 
-router.get("/update", () => {})
-router.get("/update/:id", () => {})
-router.put("/update/:id", () => {})
-router.post("/update", () => {})
-router.delete("/update/:id", () => {})
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update", () => {});
+router.delete("/update/:id", () => {});
 
 /**
  * Update Point
-*/
+ */
 
-router.get("/updatepoint", () => {})
-router.get("/updatepoint/:id", () => {})
-router.put("/updatepoint/:id", () => {})
-router.post("/updatepoint", () => {})
-router.delete("/updatepoint/:id", () => {})
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
+router.delete("/updatepoint/:id", () => {});
 
-export default router
+export default router;
