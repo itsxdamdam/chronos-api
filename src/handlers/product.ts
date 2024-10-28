616b0d8b01522dd539a9413031a05c9cc22d029f
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-10-28 14:44:48 +0100

    refactor: product handlers

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -7,13 +7,62 @@ export const getAllProducts = async (res, req) => {
       id: req.user.id,
     },
     include: {
-      product: true,
+      products: true,
     },
   });
 
-  res.json({ data: user.product });
+  res.json({ data: user.products });
 };
 
 export const getOneProduct = async (res, req) => {
-  const id = req.params.id
-}
+  const id = req.params.id;
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+      belongsToId: req.user.id,
+    },
+  });
+
+  res.json({ data: product });
+};
+
+export const createProduct = async (req, res) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      belongsToId: req.user.id,
+    },
+  });
+
+  res.json({ data: product });
+};
+
+export const updateProduct = async (req, res) => {
+  const updated = await prisma.product.update({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+    data: {
+      name: req.body.name,
+    },
+  });
+
+  res.json({ data: updated });
+};
+
+export const deleteProduct = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+  });
+
+  res.json({ data: deleted });
+};
