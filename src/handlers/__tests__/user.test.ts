41317e29bf8e1e7cfb16131c90bed4bf14fbfa99
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-11-02 23:09:35 +0100

    router test for user

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,5 +1,11 @@
+import * as user from "../user"
+
 describe("user handler", () => {
-  it("should expect something like this", () => {
-    expect(1).toBe(1)
+  it("create a new user", async () => {
+    const req = {body: {username: "hello", password: "hi"}}
+    const res = {json({token}) {
+      expect(token).toBeTruthy()
+    }}
+    await user.createNewUser(req, res, () => {})
   });
 });
